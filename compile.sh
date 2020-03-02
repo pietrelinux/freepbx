d2ab5f22d0b1b3494296b02af5395cdf70fcbc40
pietre.linux <pietre.linux@gmail.com>
2020-03-02 02:12:52 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 apt-get update && apt-get upgrade -y 
 apt-get install -y build-essential checkinstall cmake openssh-server apache2 mariadb-server\
 mariadb-client bison flex php php-curl php-cli php-pdo php-mysql php-pear php-gd php-mbstring php-intl\
-curl sox libncurses5-dev libssl-dev mpg123 libxml2-dev odbcinst libnewt-dev sqlite3\
+curl sox libncurses5-dev libssl-dev mpg123 libxml2-dev odbcinst libnewt-dev libedit-dev sqlite3\
 libsqlite3-dev pkg-config automake libtool autoconf git unixodbc-dev uuid uuid-dev\
 libasound2-dev libogg-dev libvorbis-dev libicu-dev libcurl4-openssl-dev libical-dev libneon27-dev libsrtp0-dev\
 libspandsp-dev sudo subversion libtool-bin python-dev unixodbc dirmngr sendmail-bin sendmail\
@@ -22,9 +22,9 @@ checkinstall --nodoc --pkgname "mariadb-connector-client-library" --pkgversion "
 apt install checkinstall
 checkinstall --nodoc --pkgname "mariadb-connector-client-library" --pkgversion "3.1.7" --provides "mariadb-connector-client-library" --requires "libssl1.0.2" --requires "mariadb-server" --maintainer "pietre.linux@gmail.com" --replaces none --conflicts none --install=no -y
 sudo dpkg -i mariadb-connector-client-library_3.1.7-1_arm64.deb 
-cd ..
+cd /usr/src/
 cd mariadb-connector-odbc/
-git checkout tags/3.1.6
+git checkout tags/3.0.1
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DWITH_OPENSSL=true -DCMAKE_INSTALL_PREFIX=/usr/local -LH
 make
 
